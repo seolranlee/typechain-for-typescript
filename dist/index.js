1222648b22cdf3f5b67e7cf5069f164f2cb058ef
seolranlee <leesugar90@gmail.com>
2020-08-03 18:54:54 +0900

    Validating Block Structure

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// 블록체인의 기반: 블록들이 자신의 전(previous) 블록으로의 링크가 있다는 것.
 Object.defineProperty(exports, "__esModule", { value: true });
 const CryptoJS = require("crypto-js");
 class Block {
@@ -11,6 +12,13 @@ class Block {
     }
 }
 Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => {
+    return typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+};
 const genesisBlock = new Block(0, "20237489", "", "Hello", 123456);
 let blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
@@ -24,5 +32,15 @@ const createNewBlock = (data) => {
     const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
     return newBlock;
 };
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
+// 블록체인의 기반: 블록들이 자신의 전(previous) 블록으로의 링크가 있다는 것.
+
 import * as CryptoJS from 'crypto-js';
 
 class Block {
+
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+
+    static validateStructure = (aBlock: Block): boolean => {
+        return typeof aBlock.index === "number" &&
+            typeof aBlock.hash === "string" &&
+            typeof aBlock.previousHash === "string" &&
+            typeof aBlock.timestamp === "number" &&
+            typeof aBlock.data === "string";
+    }
+
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
-
-    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
 
     constructor(
         index: number,
@@ -43,5 +60,14 @@ const createNewBlock = (data: string): Block => {
     return newBlock;
 }
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"))
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+}
+
 export { };
