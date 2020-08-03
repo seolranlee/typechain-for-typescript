3e3addb4e0a9522231eef78d7875d858863a5327
seolranlee <leesugar90@gmail.com>
2020-08-03 18:18:17 +0900

    Blockchain Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "nicolas",
-    age: 22,
-    gender: "male"
-};
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!!`;
-};
-console.log(sayHi(person));
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+const genesisBlock = new Block(0, "20237489", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Block {
 
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const person = {
-    name: "nicolas",
-    age: 22,
-    gender: "male"
+const genesisBlock: Block = new Block(0, "20237489", "", "Hello", 123456)
 
-}
+let blockchain: [Block] = [genesisBlock]
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!!`
-}
-
-console.log(sayHi(person))
+console.log(blockchain)
 
 export { };
